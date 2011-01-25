0bf53763d3474bd32bc0bd6b8e8abb9dcf0750a9
sabbir <s@gypsy.(none)>
2011-01-25 21:52:36 +0600

    little change

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php 
 //Database configuration
 define('DB_HOST','localhost');
-define('DB_USERNAME','chinky');
-define('DB_PASS','xKsyQQnUWn1C');
-define('DB_NAME','chinky_app');
+define('DB_USERNAME','');
+define('DB_PASS','');
+define('DB_NAME','');
 
 //domain
 define('SITE_NAME','');
 
 //FTP Configuration
-$ftp_server='ftp.leannewooster.com';
-$ftp_user_name='admin@leannewooster.com';
-$ftp_user_pass='11235813';
+$ftp_server='';
+$ftp_user_name='';
+$ftp_user_pass='';
 $xml_dir='./app/xmls/';
 //for ftp mention the full path
